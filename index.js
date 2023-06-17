28d6a74c95a409b8d1686cd3bff80d3422e92a11
TarifHasan <tariquzzaman.tapon@gmail.com>
2023-06-17 23:01:30 +0600

    instructor and popular class section is add

--- index.js
+++ index.js
@@ -24,13 +24,41 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    maxPoolSize: 10,
+
 });
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect((err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        })
+
+        const popularClassesCollection = client.db("summerDB").collection("popularClasses");
+        
+        const instructorsCollection = client.db("summerDB").collection("popularInstructors");
+
+        app.get('/popular', async (req, res) => {
+            const cursor = popularClassesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.get('/instructors', async (req, res) => {
+            const cursor = instructorsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
